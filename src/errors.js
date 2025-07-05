7e0d312fbc8b150f6b329d923ef3d3293b61436c
strfunctionk <comzun01@gmail.com>
2025-07-06 05:20:06 +0900

    refactor: 유저 정보 에러 핸들링

--- src/errors.js
+++ src/errors.js
@@ -7,3 +7,24 @@ export class DuplicateUserEmailError extends Error {
         this.data = data;
     }
 }
+
+export class UserNotFoundError extends Error {
+    errorCode = "U001";
+
+    constructor(reason) {
+        super(reason);
+        this.reason = reason;
+        this.statusCode = 400;
+    }
+}
+
+export class ServerError extends Error {
+    errorCode = "C001";
+
+    constructor(reason, data) {
+        super(reason);
+        this.reason = reason;
+        this.data = data;
+        this.statusCode = 500;
+    }
+}
