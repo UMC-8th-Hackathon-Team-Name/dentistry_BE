a74eb534a66f5beb415c21d3132c56ec7fb8f7ca
strfunctionk <comzun01@gmail.com>
2025-07-06 06:07:02 +0900

    fix: 삭제된 파일 복구

--- src/errors.js
+++ src/errors.js
@@ -7,6 +7,17 @@ export class DuplicateUserEmailError extends Error {
     this.data = data;
   }
 }
+
+export class UserNotFoundError extends Error {
+    errorCode = "U001";
+
+    constructor(reason) {
+        super(reason);
+        this.reason = reason;
+        this.statusCode = 400;
+    }
+}
+
 export class StationNotFoundError extends Error {
   errorCode = "S001";
   statusCode = 404;
@@ -15,4 +26,15 @@ export class StationNotFoundError extends Error {
     this.reason = reason;
     this.data = data;
   }
+}
+
+export class ServerError extends Error {
+    errorCode = "C001";
+
+    constructor(reason, data) {
+        super(reason);
+        this.reason = reason;
+        this.data = data;
+        this.statusCode = 500;
+    }
 }
