b43f7651b29f5097120293aaa2d172dc6dfe1738
정휘준 <hwijune@jeonghwijun-ui-MacBookPro.local>
2025-07-05 23:04:16 +0900

    수정중

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ export const handleUserSignUp = async (req, res, next) => {
 /*
     #swagger.tags = ['User']
     #swagger.summary = '회원가입'
-    #swagger.description = '회원가입을 위한 API입니다. 이메일, 이름, 아이디, 비밀번호를 포함해 요청해야 합니다. 선택적으로 avatar URL을 포함할 수 있습니다.'
+    #swagger.description = '회원가입을 위한 API입니다. 이메일, 비밀번호를 포함해 요청해야 합니다.'
     
     #swagger.requestBody = {
       required: true,
@@ -17,11 +17,8 @@ export const handleUserSignUp = async (req, res, next) => {
             properties: {
               email: { type: 'string', example: 'email@email.com' },
               name: { type: 'string', example: '이름' },
-              username: { type: 'string', example: '아이디' },
-              password: { type: 'string', example: '비밀번호' },
-              avatar: { type: 'string', example: 'https://example.com/avatar.png' }
             },
-            required: ['email', 'name', 'username', 'password']
+            required: ['email','password']
           }
         }
       }
@@ -41,8 +38,6 @@ export const handleUserSignUp = async (req, res, next) => {
                 properties: {
                   id: { type: 'number', example: 1 },
                   email: { type: 'string', example: 'email@email.com' },
-                  name: { type: 'string', example: '이름' },
-                  username: { type: 'string', example: '아이디' },
                   password: { type: 'string', example: '비밀번호' },
                   avatar: { type: 'string', example: 'https://example.com/avatar.png' },
                   createdAt: { type: 'string', example: '2023-01-01T00:00:00.000Z' },
@@ -101,14 +96,4 @@ export const handleUserSignUp = async (req, res, next) => {
       }
     }
 */
-
-    try {
-        console.log("회원가입을 요청했습니다!");
-        console.log("body:", req.body); // 값이 잘 들어오나 확인하기 위한 테스트용
-
-        const user = await userSignUp(bodyToUser(req.body));
-        res.status(StatusCodes.OK).success(user);
-    } catch (err) {
-        return next(err);
-    }
 };
